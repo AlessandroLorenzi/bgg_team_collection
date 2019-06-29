3571b59f9c12f94139c8c6a3b3c14c50b7b1a0e8
Alessandro Lorenzi <alorenzi@alorenzi.eu>
2019-06-29 19:36:04 +0200

    Sort by rating

--- site/js/scripts.js
+++ site/js/scripts.js
@@ -1,3 +1,15 @@
+var games = []
+var players = []
+
+function fetch_games (){
+    $.getJSON( "games.json", function( data ) {
+        games = data['games'];
+        players = data['players'];
+        sort_games_by_rate()
+        reload_games();
+    });
+}
+
 function reload_games(){
     document.getElementById("games").innerHTML= "";
 
@@ -7,18 +19,16 @@ function reload_games(){
 
     var e = document.getElementById("players");
     var players = parseInt(e.value);
+    for (var i in games){
+        game = games[i];
+        if
+        ( (game['owner'] == owner || owner == '') &&
+          (!(players >0) || (parseInt(game['stats']['minplayers'])<= players && players <= parseInt(game['stats']['maxplayers'])) )
+        ){
+            put_games(game);
+        }
+    }
 
-
-    $.getJSON( "games.json", function( data ) {
-        data['games'].forEach(function(game){
-            if
-            ( (game['owner'] == owner || owner == '') &&
-              (!(players >0) || (parseInt(game['stats']['minplayers'])<= players && players <= parseInt(game['stats']['maxplayers'])) )
-            ){
-                put_games(game);
-            }
-        });
-    });
 }
 
 
@@ -29,9 +39,24 @@ function put_games(game){
     div_game.innerHTML += "<img src=\""+ game['thumbnail']+"\"/>";
     div_game.innerHTML += "<p>";
     div_game.innerHTML += "Proprietario: "+ game['owner'] +"</br>";
+    div_game.innerHTML += "Rating: "+ game['rating'] +"</br>";
     div_game.innerHTML += "</p>";
     document.getElementById("games").appendChild(div_game);  
 }
 
+function sort_games_by_rate(){
+    sorted_games = [];
+    while (games.length != 0) {
+        alpha = games.pop();
+        for(var i=0; i < games.length; i++){
+            beta = games[i]
+            if(parseFloat(alpha.rating) < parseFloat(beta.rating)){
+                alpha = [games[i], games[i]=alpha][0]; 
+            }
+        }
+        sorted_games.push(alpha);
+    }
+    games = sorted_games;
+}
 
-reload_games()
+fetch_games()
